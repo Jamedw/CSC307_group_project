b8eeddbcac11264ba97a2618c02d4c4db916d1d4
jamedw <jamesdwyer771@gmail.com>
2024-05-09 21:33:34 -0700

    started Landing page

---
+++ packages/react-frontend/src/pages/Home.jsx
@@ -0,0 +1,49 @@
+function VideoList({ videos, emptyHeading }) {
+    const count = videos.length;
+    let heading = emptyHeading;
+    if (count > 0) {
+    const noun = count > 1 ? 'Videos' : 'Video';
+    heading = count + ' ' + noun;
+    }
+    return (
+    <section>
+        <h2>{heading}</h2>
+        {videos.map(video =>
+        <Video key={video.id} video={video} />
+        )}
+    </section>
+    );
+}
+
+function Video({ video }) {
+    return (
+      <div>
+
+        <a href={video.url}>
+          <h3>{video.title}</h3>
+          <p>{video.description}</p>
+        </a>
+
+      </div>
+    );
+  }
+function Login()
+{
+    return(
+        <button>
+            Log in
+        </button>
+    );
+}
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Login />
+        </div>
+    );
+  };
+  
+  
+  export default Home;
