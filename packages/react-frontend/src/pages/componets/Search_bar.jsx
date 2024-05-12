286b776120d8d1551e564e642b8d8f15f37455b2
jamedw <jamesdwyer771@gmail.com>
2024-05-11 17:42:27 -0700

    changes to Home.css

--- packages/react-frontend/src/pages/componets/Search_bar.jsx
+++ packages/react-frontend/src/pages/componets/Search_bar.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { even } from "prelude-ls";
+import { React , useState} from "react";
+
+const defaultval = "Search for Post or Community";
+
+function formSubmit(e)
+{
+    console.log(e.target.value);
+    e.target.value = "";
+}
+function resetform(e)
+{
+    if (e.target.value === defaultval)
+    e.target.value = ""
+}
+function handleKeyPress(event)
+{
+    if (event.key === 'Enter') {
+    console.log(event.target.value)
+    event.target.value = ""
+    }
+}
 
 export default function search()
 {
     return(
-        <input name="Community_Post" defaultValue="Search for Post or Community" 
-        onFocus={(e) => e.target.value = ""} size={60}/>
+        <input name="Community_Post" defaultValue={defaultval}
+        onFocus={(e) => resetform(e)}
+        onKeyPress={(e) => handleKeyPress(e)}       
+        size={80}
+        />
     );
 }
