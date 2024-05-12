fa7cbccf69b32aac5462f8c66eb6429210064d7c
chris <chmontiel@calpoly.edu>
2024-05-11 20:41:11 -0700

    added names

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,12 +4,66 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+
+
 app.use(express.json());
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
