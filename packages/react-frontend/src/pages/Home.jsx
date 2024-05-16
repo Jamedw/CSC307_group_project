c186fa92483879a0269f1b18f440e2177f8d2a24
jamedw <jamesdwyer771@gmail.com>
2024-05-15 20:45:23 -0700

    update to post.jsx

--- packages/react-frontend/src/pages/Home.jsx
+++ packages/react-frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import Sidebar from "./Sidebar";
 import React, {useState} from "react";
-import Posts from "./Posts"
+import Posts from "./componets/Posts"
 import Navbar from "./componets/Navbar";
 import "./Home.css"
+import logo from "./images/stanchevmeme.png"
 
 function Home() {
 
@@ -10,8 +11,11 @@ function Home() {
         profile: "test",
         header: "test"
     },
-{profile: "test",
-header: "test"},{profile: "test",
+{postimg: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngitem.com%2Fpimgs%2Fm%2F52-525476_pepe-frog-rana-triste-sad-frog-meme-hd.png&f=1&nofb=1&ipt=e6fc08fed5b06537776741c520d85296f1afadf85b53c0018e4e46a8bd4137bd&ipo=images" ,
+username: "frog man", userprofile:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngitem.com%2Fpimgs%2Fm%2F52-525476_pepe-frog-rana-triste-sad-frog-meme-hd.png&f=1&nofb=1&ipt=e6fc08fed5b06537776741c520d85296f1afadf85b53c0018e4e46a8bd4137bd&ipo=images" ,community: "thefrogmancommunity",
+header: "frog man returns?"},{postimg: logo ,
+username: "Lubomir Stanchev", userprofile: logo, community: "DBMSs",
+header: "OMG!!! Yo stanchev is calling and its 3am"},{profile: "test",
 header: "test"},{header: "test"},{profile: "test",
 header: "test"},{profile: "test",
 header: "test"},{profile: "test",
@@ -46,6 +50,9 @@ header: "test"}]);
                 </div>
                 <div class="main">
                     <Posts postData={posts}/>
+                </div>
+                <div>
+                    
                 </div>
             </div>
             
