902914d327f99ec29c20ddb4b3360d3e4bb386d2
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-05-23 22:11:44 -0700

    updated model/users

---
+++ packages/express-backend/services/user-Ref.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import userModel from "../models/users.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose.set("debug", true);
+mongoose   
+	.connect(process.env.MONGODB_URI)   
+	.catch((error) => console.log(error));  
+
+async function getUsers(name, job) {
+	let users;
+
+	if (name && job) {
+		users = await findUserByNameAndJob(name, job);   
+    }
+	else if (name && !job) {
+		users = await findUserByName(name);   
+	} else if (job && !name) {
+		users = await findUserByJob(job);   
+	}
+    else {
+        users = await userModel.find();
+    }
+	return users; 
+}  
+
+async function findUserById(id) {
+	return await userModel.findById(id); 
+}  
+
+async function findUserByIdAndDelete(id) {
+	return await userModel.findByIdAndDelete(id);
+}
+
+async function addUser(user) {
+	const userToAdd = new userModel(user);	
+	return await userToAdd.save();
+}  
+
+async function findUserByName(name) {
+	return await userModel.find({ name: name }); 
+}  
+
+async function findUserByJob(job) {
+	return await userModel.find({ job: job }); 
+}  
+
+async function findUserByNameAndJob(name, job) {
+	return await userModel.find({ name: name, job: job });
+}
+
+export {   
+	addUser,   
+	getUsers,   
+	findUserById,   
+	findUserByName,   
+	findUserByJob,
+    findUserByNameAndJob,
+    findUserByIdAndDelete,
+};
--- packages/express-backend/services/user-service.js
+++ packages/express-backend/services/user-service.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import userModel from "../models/users.js";
+import postsModel from "../models/users.js";
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -9,54 +9,4 @@ mongoose
 	.connect(process.env.MONGODB_URI)   
 	.catch((error) => console.log(error));  
 
-async function getUsers(name, job) {
-	let users;
 
-	if (name && job) {
-		users = await findUserByNameAndJob(name, job);   
-    }
-	else if (name && !job) {
-		users = await findUserByName(name);   
-	} else if (job && !name) {
-		users = await findUserByJob(job);   
-	}
-    else {
-        users = await userModel.find();
-    }
-	return users; 
-}  
-
-async function findUserById(id) {
-	return await userModel.findById(id); 
-}  
-
-async function findUserByIdAndDelete(id) {
-	return await userModel.findByIdAndDelete(id);
-}
-
-async function addUser(user) {
-	const userToAdd = new userModel(user);	
-	return await userToAdd.save();
-}  
-
-async function findUserByName(name) {
-	return await userModel.find({ name: name }); 
-}  
-
-async function findUserByJob(job) {
-	return await userModel.find({ job: job }); 
-}  
-
-async function findUserByNameAndJob(name, job) {
-	return await userModel.find({ name: name, job: job });
-}
-
-export {   
-	addUser,   
-	getUsers,   
-	findUserById,   
-	findUserByName,   
-	findUserByJob,
-    findUserByNameAndJob,
-    findUserByIdAndDelete,
-};
