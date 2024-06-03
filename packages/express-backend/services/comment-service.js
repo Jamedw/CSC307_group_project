7d613012b91bf360bd3d51dca2ff194f94920d9e
Brendan Holt <holt0316@gmail.com>
2024-06-03 10:00:31 -0700

    commented out function

--- packages/express-backend/services/comment-service.js
+++ packages/express-backend/services/comment-service.js
@@ -30,35 +30,30 @@ async function getCommentById(commentId) {
     }
 }
 
-async function createComment(req, commentData) {
+/*async function createComment(req, commentData) {
 
-    try {
         const user = req.user; // User should be attached by the middleware
 
-        // Ensure required fields are present
+
         if (!commentData.content) {
             throw new Error("Content is required");
         }
 
-        // Create the new comment
         const newComment = new commentModel({
             username: user.username,
             content: commentData.content
         });
 
-        // Save the comment
+
         const savedComment = await newComment.save();
 
-        // Update user's commentIds
+
         user.commentIds.push(savedComment._id);
         await user.save();
 
         return savedComment;
-    } catch (error) {
-        console.error("Error creating comment:", error);
-        throw error;
-    }
-}
+
+}*/
 
 /*TODO Make a post to create a comment. Required fields are userName
 and content.
