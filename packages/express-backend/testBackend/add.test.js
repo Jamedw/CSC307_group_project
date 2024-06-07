a65271a2e91a5e77f042ac5ba52ca2852e013bf0
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-06-07 02:11:53 -0700

    deleted the trial add and sub tests

--- packages/express-backend/testBackend/add.test.js
+++
@@ -1,5 +0,0 @@
-const add = require('./add');
-
-test('add two numbers', () => {
-    expect(add(1, 2)).toBe(3)
-})
--- packages/express-backend/testBackend/sub.test.js
+++
@@ -1,5 +0,0 @@
-const sub = require('./sub');
-
-test('subtract two numbers', () => {
-    expect(sub(1, 2)).toBe(-1)
-})
