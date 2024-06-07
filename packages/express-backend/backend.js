80cf3b9ad1dc1845793bbb054e3159c356f1aba3
jamedw <jamesdwyer771@gmail.com>
2024-06-07 10:23:29 -0700

    update

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 // Start the server
 app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+  console.log(`backend app is listening`);
 });
 
 app.get('/', (req, res) => {
